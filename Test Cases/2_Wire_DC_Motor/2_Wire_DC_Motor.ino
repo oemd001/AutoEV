f855728db9806c860881c2c200dfa9febc4d0d2d
oemd001 <kennethchen2001@gmail.com>
2020-08-04 02:25:46 -0700

    Changed Left Motor and Right Motor direction. Added README Section

--- Test Cases/2_Wire_DC_Motor/2_Wire_DC_Motor.ino
+++ Test Cases/2_Wire_DC_Motor/2_Wire_DC_Motor.ino
@@ -1,5 +1,14 @@
-const int pin1 = 2;
-const int pin2 = 3;
+/*  README FIRST!
+ *  Prior to running this program, you must have the following:
+ *  2x PN2222 Transistor
+ *  1x 1N4001 diode
+ *  1x 270 Ω Resistor
+ *  Without these materials, the DC motor will not work when plugged in to it's designated pin. 
+ *  HOWEVER, it will run via 5V DC. 
+ */
+
+const int pin1 = 13; //right Motor
+const int pin2 = 12; //left Motor
 
 void setup() {
   // put your setup code here, to run once
@@ -10,5 +19,5 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(pin1, HIGH);
-  digitalWrite(pin2, HIGH);
+  digitalWrite(pin2, HIGH); //Different orientations. 
 }
