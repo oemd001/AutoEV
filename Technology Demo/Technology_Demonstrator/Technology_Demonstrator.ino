b7044f1440e53a0cc5ef2b5cf4997a02c536c00d
oemd001 <kennethchen2001@gmail.com>
2020-08-05 01:37:20 -0700

    completed Ultrasonic sensor setup

--- Technology Demo/Technology_Demonstrator/Technology_Demonstrator.ino
+++ Technology Demo/Technology_Demonstrator/Technology_Demonstrator.ino
@@ -15,22 +15,93 @@ const int LEFT_TRIG = 9;
 const int LEFT_MOTOR = 12;
 const int RIGHT_MOTOR = 13;
 
-int distance = 30;
+//Sensor detection variables
+//Back Sensor Variables
+double bdistance;
+double bduration;
+//Front Sensor Variables
+double fdistance;
+double fduration;
+//Left Sensor Variables
+double ldistance;
+double lduration;
+//Right Sensor Variables
+double rdistance;
+double rduration;
+
+//debug LED
+int ledPin = 13;
 
 void setup() {
   //Init output/input pins
-  pinMode(BACK_ECHO, OUTPUT);
+  Serial.begin(9600);
+  pinMode(BACK_ECHO, INPUT);
   pinMode(BACK_TRIG, OUTPUT);
-  pinMode(FRONT_ECHO, OUTPUT);
+  pinMode(FRONT_ECHO, INPUT);
   pinMode(FRONT_TRIG, OUTPUT);
-  pinMode(RIGHT_ECHO, OUTPUT);
+  pinMode(RIGHT_ECHO, INPUT);
   pinMode(RIGHT_TRIG, OUTPUT);
-  pinMode(LEFT_ECHO, OUTPUT);
+  pinMode(LEFT_ECHO, INPUT);
   pinMode(LEFT_TRIG, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //back facing UltraSonic sensor loop
+  //digitalWrite(ledPin, LOW);
+  digitalWrite(BACK_TRIG, HIGH);
+  delay(50);
+  digitalWrite(BACK_TRIG, LOW);
+  bduration = pulseIn(BACK_ECHO, HIGH);
+  bdistance = (bduration/2)/29.1;
+  if (bdistance <= 10) {
+    //digitalWrite(ledPin, HIGH);
+    delay(50);
+    Serial.println("Obstacle detected @ back ");
+    Serial.println(bdistance);
+    Serial.println("cm");
+  }
 
+  //forward facing UltraSonic sensor loop
+  digitalWrite(FRONT_TRIG, HIGH);
+  delay(50);
+  digitalWrite(FRONT_TRIG, LOW);
+  fduration = pulseIn(FRONT_ECHO, HIGH);
+  fdistance = (fduration/2)/29.1;
+  if (fdistance <= 10) {
+    //digitalWrite(ledPin, HIGH);
+    delay(50);
+    Serial.println("Obstacle detected @ front ");
+    Serial.println(fdistance);
+    Serial.println("cm");
+  }
 
+  //Left facing Ultrasonic sensor loop
+  digitalWrite(LEFT_TRIG, HIGH);
+  delay(50);
+  digitalWrite(LEFT_TRIG, LOW);
+  lduration = pulseIn(LEFT_ECHO, HIGH);
+  ldistance = (lduration/2)/29.1;
+  if (ldistance <= 10) {
+    //digitalWrite(ledPin, HIGH);
+    delay(50);
+    Serial.println("Obstacle detected @ left ");
+    Serial.println(ldistance);
+    Serial.println("cm");
+  }
+
+  //Right facing Ultrasonic sensor loop
+  digitalWrite(RIGHT_TRIG, HIGH);
+  delay(50);
+  digitalWrite(RIGHT_TRIG, LOW);
+  rduration = pulseIn(RIGHT_ECHO, HIGH);
+  rdistance = (rduration/2)/29.1;
+  if (rdistance <= 10) {
+    //digitalWrite(ledPin, HIGH);
+    delay(50);
+    Serial.println("Obstacle detected @ right ");
+    Serial.println(rdistance);
+    Serial.println("cm");
+  }
+  
+  
 }
--- Test Cases/UltraSonic_Sensor_Test.ino/UltraSonic_Sensor_Test.ino.ino
+++ Test Cases/UltraSonic_Sensor_Test.ino/UltraSonic_Sensor_Test.ino.ino
@@ -1,7 +1,7 @@
 #include <NewPing.h>// Imports the NewPing Library.
-int ledPin =(13); // Add the onboard LED on pin 13.
-int trigPin = (10); // Add the Trig pin on pin 10.
-int echoPin = (9); // Add the ECHO pin on pin 9.
+int ledPin = 13; // Add the onboard LED on pin 13.
+int trigPin = 9; // Add the Trig pin on pin 10.
+int echoPin = 8; // Add the ECHO pin on pin 9.
 int duration, distance; // Add types 'duration' and 'distance'.
 
 void setup() {
@@ -18,12 +18,14 @@ void loop() {
   digitalWrite (trigPin, LOW);
   duration=pulseIn(echoPin,HIGH);
   distance=(duration/2)/29.1;
-  if(distance <=30) // If the sensor detects an obstacle less than 30 cm in distance, the LED will start to blink.
+  if(distance <=30) {// If the sensor detects an obstacle less than 30 cm in distance, the LED will start to blink.
   digitalWrite (ledPin, HIGH);
   delay(50);
-  if(distance >= 100)// If no obstacle is there within 30 cm, the Led should turn off.
+  }
+  if(distance >= 100) { // If no obstacle is there within 30 cm, the Led should turn off.
   digitalWrite (ledPin, LOW);
   delay(50);
+  }
   Serial.print("cm"); 
   Serial.println(distance);
 }
