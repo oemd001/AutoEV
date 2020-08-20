42cc042ed7245f12634ca55daa2975926c43ed67
oemd001 <kennethchen2001@gmail.com>
2020-08-20 01:21:17 -0700

    Added another video

--- Test Cases/Python OpenCV/Lane_Detection/Static_Lane_Detectoin.py
+++ Test Cases/Python OpenCV/Lane_Detection/Static_Lane_Detectoin.py
@@ -1,49 +1,118 @@
 import cv2
-import numpy as np 
+import numpy as np
 
-# load image
-image = cv2.imread('pw12b.jpg')
-# crop image
-h,w = image.shape[:2]
-image = image[200:h-20,20:550]
-# create hsv
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-# set lower and upper color limits
-low_val = (0,0,0)
-high_val = (179,45,96)
-# Threshold the HSV image 
-mask = cv2.inRange(hsv, low_val,high_val)
-# remove noise
-mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=np.ones((8,8),dtype=np.uint8))
-# close mask
-mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel=np.ones((20,20),dtype=np.uint8))
+frame = cv2.imread('pic.jpg')
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-# improve mask by drawing the convexhull 
-ret, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-for cnt in contours:
-    hull = cv2.convexHull(cnt)
-    cv2.drawContours(mask,[hull],0,(255), -1)
-# erode mask a bit to migitate mask bleed of convexhull
-mask = cv2.morphologyEx(mask, cv2.MORPH_ERODE, kernel=np.ones((5,5),dtype=np.uint8))
+lower_blue = np.array([60, 40, 40])
+upper_blue = np.array([150, 255, 255])
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
+edges = cv2.Canny(mask, 200, 400)
 
-# remove this line, used to show intermediate result of masked road
-road = cv2.bitwise_and(image, image,mask=mask)
+def detect_edges(frame):
+    # filter for blue lane lines
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    show_image("hsv", hsv)
+    lower_blue = np.array([60, 40, 40])
+    upper_blue = np.array([150, 255, 255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    show_image("blue mask", mask)
+    # detect edges
+    edges = cv2.Canny(mask, 200, 400)
+    return edges
 
-# apply mask to hsv image
-road_hsv = cv2.bitwise_and(hsv, hsv,mask=mask)
-# set lower and upper color limits
-low_val = (0,0,102)
-high_val = (179,255,255)
-# Threshold the HSV image 
-mask2 = cv2.inRange(road_hsv, low_val,high_val)
-# apply mask to original image
-result = cv2.bitwise_and(image, image,mask=mask2)
+def region_of_interest(edges):
+    height, width = edges.shape
+    mask = np.zeros_like(edges)
+    # only focus bottom half of the screen
+    polygon = np.array([[
+        (0, height * 1 / 2),
+        (width, height * 1 / 2),
+        (width, height),
+        (0, height),
+    ]], np.int32)
+    cv2.fillPoly(mask, polygon, 255)
+    cropped_edges = cv2.bitwise_and(edges, mask)
+    return cropped_edges
 
-#show image
-cv2.imshow("Result", result)
-cv2.imshow("Road", road)
-cv2.imshow("Mask", mask)
-cv2.imshow("Image", image)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def detect_line_segments(cropped_edges):
+    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+    min_threshold = 10  # minimal of votes
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, 
+                                    np.array([]), minLineLength=8, maxLineGap=4)
+
+    return line_segments
+def average_slope_intercept(frame, line_segments):
+    """
+    This function combines line segments into one or two lane lines
+    If all line slopes are < 0: then we only have detected left lane
+    If all line slopes are > 0: then we only have detected right lane
+    """
+    lane_lines = []
+    if line_segments is None:
+        logging.info('No line_segment segments detected')
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+    boundary = 1/3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+    return lane_lined
+
+def make_points(frame, line):
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    return [[x1, y1, x2, y2]]
+
+def detect_lane(frame):
+    edges = detect_edges(frame)
+    cropped_edges = region_of_interest(edges)
+    line_segments = detect_line_segments(cropped_edges)
+    lane_lines = average_slope_intercept(frame, line_segments)  
+    return lane_lines
+
+
+def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
+    line_image = np.zeros_like(frame)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
+    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return line_image
+
+lane_lines_image = display_lines(frame, lane_lined)
+cv2.imshow("lane lines", lane_lines_image)
