6fe3318fe8c6ebb67bd43e70b9eb1bc6b9cec4e0
oemd001 <kennethchen2001@gmail.com>
2020-08-21 00:27:57 -0700

    Added HLS image filtration

--- Test Cases/Python OpenCV/Sample_Lane_Detection/Image_Proc.py
+++ Test Cases/Python OpenCV/Sample_Lane_Detection/Image_Proc.py
@@ -4,10 +4,11 @@
 import cv2 as cv
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 import os
 
-# Adding first test image to convert default RGB to a 'computer readable' BGR
+# If you only have one image, uncomment this code. It's better to have multiple to have better results from the ML
 # img = cv.imread("image.jpg")
 # img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
 
@@ -18,6 +19,7 @@ imgList = []
 for i in range(len(imgFiles)):
     imgList.append(mpimg.imread(imgDir + imgFiles[i])) # mpimg is part of matplotlib
 
+# Run this method to test whether the program actually detects the given images. 
 def displayImage(images, cmap = None):
     plt.figure(figsize=(40, 40))
     for i, image in enumerate(images):
@@ -26,9 +28,18 @@ def displayImage(images, cmap = None):
         plt.autoscale(tight = True)
     plt.show()
 
+# HLS color filtering.
+# BGR is uncesseary as HLS removes all extraneous colors
+def filterImage(image):
+    hls = cv.cvtColor(image, cv.COLOR_RGB2HLS)
+    whiteLower = np.array([0, 190, 0])
+    whiteUpper = np.array([255, 255, 255])
+    yellowLower = np.array([10, 0, 90])
+    yellowUpper = np.array([50, 255, 255])
+    yellowMask = cv.inRange(hls, yellowLower, yellowUpper)
+    whiteMask = cv.inRange(hls, whiteLower, whiteUpper)
+    mask = cv.bitwise_or(yellowMask, whiteMask)
+    maskResult = cv.bitwise_and(image, image, mask = mask)
+    return maskResult
+
 displayImage(imgList)
-
-
-
-
-
