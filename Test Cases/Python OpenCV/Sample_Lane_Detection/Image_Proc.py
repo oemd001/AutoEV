9e5411dc779f616b8510e15478ade9f3603e0566
oemd001 <kennethchen2001@gmail.com>
2020-08-21 00:06:35 -0700

    imported matplotlib as matplotlib.pyploy\t

--- Test Cases/Python OpenCV/Sample_Lane_Detection/Image_Proc.py
+++ Test Cases/Python OpenCV/Sample_Lane_Detection/Image_Proc.py
@@ -2,18 +2,21 @@
 
 # Import necessary libraries
 import cv2 as cv
-import matplotlib
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import sys
+import os
 
 # Adding first test image to convert default RGB to a 'computer readable' BGR
-img = cv.imread("image.jpg")
-img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+# img = cv.imread("image.jpg")
+# img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
 
 # Adding the list of images to imgFiles via the append method. 
 imgDir = "Test_Images/"
 imgFiles = os.listdir(imgDir)
 imgList = []
 for i in range(len(imgFiles)):
-    imgList.append(mpimg.imgread(imgDir + imgFiles[i])) # mpimg is part of matplotlib
+    imgList.append(mpimg.imread(imgDir + imgFiles[i])) # mpimg is part of matplotlib
 
 def displayImage(images, cmap = None):
     plt.figure(figsize=(40, 40))
@@ -23,7 +26,7 @@ def displayImage(images, cmap = None):
         plt.autoscale(tight = True)
     plt.show()
 
-displayImage(imageList)
+displayImage(imgList)
 
 
 
