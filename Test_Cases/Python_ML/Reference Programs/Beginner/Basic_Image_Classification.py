c7685437715e010efb562de529e3c842c0ef716f
oemd001 <kennethchen2001@gmail.com>
2020-08-24 04:42:05 -0400

    Started Basic Image Classification ML Tutorial

---
+++ Test_Cases/Python_ML/Reference Programs/Beginner/Basic_Image_Classification.py
@@ -0,0 +1,10 @@
+# Critical packages. run 'pip3 install tensorflow' if Python doesn't recognize
+# tensorflow. 
+import tensorflow as tf
+from tensorflow import keras
+
+# Important packages. Make sure to include this.
+import matplotlib.pyplot as plt
+import numpy as np
+
+fashion_mnist = keras.datasets.fashion_mnist
