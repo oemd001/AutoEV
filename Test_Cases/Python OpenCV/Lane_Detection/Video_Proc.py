e09ab1ff0169c9340c5b615cc63608a647115098
System Administrator <root@Doriss-iMac.local>
2020-09-05 22:54:23 -0700

    Added perspective warping

--- Test_Cases/Python OpenCV/Lane_Detection/Video_Proc.py
+++ Test_Cases/Python OpenCV/Lane_Detection/Video_Proc.py
@@ -28,6 +28,69 @@ def displayImage(images, cmap = None):
         plt.autoscale(tight = True)
     plt.show()
 
+def pipeline(img, s_thresh=(100, 255), sx_thresh=(15, 255)):
+    img = undistort(img)
+    img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv.cvtColor(img, cv.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hls[:,:,1]
+    s_channel = hls[:,:,2]
+    h_channel = hls[:,:,0]
+    # Sobel x
+    sobelx = cv.Sobel(l_channel, cv.CV_64F, 1, 1) # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+    
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    
+    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+    
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+    return combined_binary
+
+def perspective_warp(img, 
+                     dst_size=(1280,720),
+                     src=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)]),
+                     dst=np.float32([(0,0), (1, 0), (0,1), (1,1)])):
+    img_size = np.float32([(img.shape[1],img.shape[0])])
+    src = src* img_size
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result 
+    # again, not exact, but close enough for our purposes
+    dst = dst * np.float32(dst_size)
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    warped = cv.warpPerspective(img, M, dst_size)
+    return warped
+
+def inv_perspective_warp(img, 
+                     dst_size=(1280,720),
+                     src=np.float32([(0,0), (1, 0), (0,1), (1,1)]),
+                     dst=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)])):
+    img_size = np.float32([(img.shape[1],img.shape[0])])
+    src = src* img_size
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result 
+    # again, not exact, but close enough for our purposes
+    dst = dst * np.float32(dst_size)
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    warped = cv.warpPerspective(img, M, dst_size)
+    return warped
+
+def get_hist(img):
+    hist = np.sum(img[img.shape[0]//2:,:], axis=0)
+    return hist
+
 # HLS color filtering.
 # BGR is uncesseary as HLS removes all extraneous colors
 def filterImage(image):
@@ -131,7 +194,7 @@ def imageProcess(image):
     cannyImage = canny(imageInterest)
     imageLine = hough_lines(cannyImage, 1, np.pi/180, 5, 20, 5)
     result = cv.addWeighted(imageLine, 1, image, 0.8, 0)
-    return result
+    return imageFilter
 
 def showVideo():
     if videoCapture.isOpened():
